f7c5f8346520ecd3c230dfc713aa2655bd23b3ab
Valentyn Solomko <Valentyn.Solomko@portaone.com>
2019-01-04 14:19:21 +0200

    Adapt for building with Gradle

--- src/main/resources/project/manifest.pl
+++
@@ -1,6 +0,0 @@
-@files = (
-    ['//step[stepName="createBrakemanCommand"]/command', 'brakemanDriver.pl'],    
-    ['//property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-    ['//property[propertyName="postp_matchers"]/value', 'postp_matchers.pl'],
-	['//procedure[procedureName="runBrakeman"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'runBrakemanForm.xml'],
-);
